4123945f4258e9397ea049bf73f2380833ee7138
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-03 22:24:31 +0530

    added functions to accept name and email inputs and POST the values in the resource located by the url

--- crud_reactjs/src/Components/Create.js
+++ crud_reactjs/src/Components/Create.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 export default function Create() {
-return (
-<div className="container m-5">
-    <div className="row">
-        <div className="col-12">
-                <form>
-                    <h3 >Create</h3>
-                <label htmlFor="inputName" className="form-label">Name</label>
-                <input type="text" id="inputName" className="form-control" />
-                <br/>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('')
+    const getName = (e) => {
+        setName(e.target.value);
+    }
+
+    const getEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        const header = { "Access-Control-Allow-Origin": "*" };
+
+        (axios.post('https://633af5ed471b8c39557973b2.mockapi.io/crud_reactjs', {
+            name,
+            email,
+            header,
+        })).catch((e) => {
+            console.log(e);
+        })
+        console.log("posted");
+    }
+    return (
+        <div className="container m-5">
+            <div className="row">
+                <div className="col-12">
+                    <form onSubmit={handleForm}>
+                        <h3 >Create</h3>
+                        <label htmlFor="inputName" className="form-label">Name</label>
+                        <input type="text" id="inputName" value={name} className="form-control" onChange={getName} />
+                        <br />
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" value={email} aria-describedby="emailHelp" onChange={getEmail} />
+                        </div>
+                        <br />
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
                 </div>
-                <br />
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            </div>
         </div>
-    </div>
-</div>
-);
+    );
 }
