a85b8eddefc611f0ff0fa3d00ce3d948b4394d11
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-05 02:01:40 +0530

    added button with Link to read component, trim() to remove white spaces from input values

--- crud_reactjs/src/Components/Create.js
+++ crud_reactjs/src/Components/Create.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Create() {
     const [name, setName] = useState('');
@@ -19,8 +19,8 @@ export default function Create() {
         const header = { "Access-Control-Allow-Origin": "*" };
 
         axios.post('https://633af5ed471b8c39557973b2.mockapi.io/crud_reactjs', {
-            name,
-            email,
+            name : name.trim(),
+            email : email.trim(),
             header,
         }).then(() => {
             navigate('/read');
@@ -29,21 +29,33 @@ export default function Create() {
         })
     }
     return (
-            <div className="row">
-                <div className="col-12">
-                    <form onSubmit={handleForm}>
-                        <h3 >Create</h3>
-                        <label htmlFor="inputName" className="form-label">Name</label>
-                        <input type="text" id="inputName" value={name} className="form-control" onChange={getName} />
-                        <br />
-                        <div className="mb-3">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" value={email} aria-describedby="emailHelp" onChange={getEmail} />
-                        </div>
-                        <br />
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
+    <div className="row">
+        <div className="col-12">
+            <form onSubmit={handleForm}>
+                <h3>Create</h3>
+                <label htmlFor="inputName" className="form-label">
+                    Name
+                </label>
+                <input type="text" id="inputName" value={name} className="form-control" onChange={getName} />
+                <br />
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" value={email}
+                        aria-describedby="emailHelp" onChange={getEmail} />
                 </div>
-            </div>
+                <br />
+                <div className="d-flex justify-content-between">
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                    <button type="submit" className="btn btn-primary">
+                        <Link to={"/read"} className="text-decoration-none text-light">
+                        Read
+                        </Link>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
     );
 }
