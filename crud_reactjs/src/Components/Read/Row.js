0acc120fd403bb68a662de5657f2e9e2fb3c076a
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-04 23:55:08 +0530

    added eventhandlers for edit and delete button

--- crud_reactjs/src/Components/Read/Row.js
+++ crud_reactjs/src/Components/Read/Row.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-export default function Row({ id, name, email }) {
+export default function Row(props) {
+  const { renderId, objectId, name, email, deleteData, updateData} = props;
   return (
     <>
       <tr>
-        <th scope="row">{id}</th>
+        <th scope="row">{renderId}</th>
         <td>{name}</td>
         <td>{email}</td>
         <td>
-          <button className="bg-primary border-white">Edit</button>
+          <button className="bg-primary border-white" onClick={()=>updateData(objectId,name,email)}>Edit</button>
         </td>
         <td>
-          <button className="bg-danger border-white">Delete</button>
+          <button className="bg-danger border-white" onClick={()=>deleteData(objectId)}>Delete</button>
         </td>
       </tr>
     </>
